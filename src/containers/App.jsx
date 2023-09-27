78e1a61bfda7a10795110eff6d1fcbce8e42f9f8
frdrk00 <masterkooo@gmail.com>
2023-09-27 18:17:30 +0200

    MenuItem

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import Header from './Header'
 
 const App = () => {
   return (
-    <div className="flex item-center justify-center">
-      <h1 className="text-blue-300">Hi there</h1>
+    <div className="w-full xl:w-[1600px] py-32 px-4 lg:px-12 pr-4 lg:pr-32">
+      {/* Particles Container */}
+
+      {/* Header */}
+      <Header />
+
+      {/* Home Container */}
+
+      {/* About Container */}
+
+      {/* Skills Container */}
+
+      {/* Projects Container */}
+
+      {/* Contact Container */}
+
+      {/* Footer */}
     </div>
   )
 }
