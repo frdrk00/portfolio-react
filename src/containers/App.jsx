fc06c63c15bc443b5109013b187501bc2b4c2faf
frdrk00 <masterkooo@gmail.com>
2023-09-27 16:59:03 +0200

    Setup - Default

---
+++ src/containers/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className="flex item-center justify-center">
+      <h1 className="text-blue-300">Hi there</h1>
+    </div>
+  )
+}
+
+export default App
---
+++ src/utils/helper.js
@@ -0,0 +1,143 @@
+import {
+  FaDiagramProject,
+  FaEnvelope,
+  FaFacebookF,
+  FaGithub,
+  FaHouse,
+  FaLinkedinIn,
+  FaRenren,
+  FaUser,
+  FaYoutube,
+} from 'react-icons/fa6'
+import {
+  CarRent,
+  BookingApp,
+  ECommerce,
+  FoodApp,
+  TrelloApp,
+  RealtimeChat,
+  TravelGoApp,
+  PixabayClone,
+  RedditClone,
+  SearchJobApp,
+  AirbnbClone,
+  LinkedInClone,
+  SpotifyClone,
+} from '../assets'
+
+export const Socials = [
+  {
+    id: `facebook-${Date.now()}`,
+    Icon: FaFacebookF,
+    uril: '',
+    color: '#1877F2',
+  },
+  {
+    id: `linkedin-${Date.now()}`,
+    Icon: FaLinkedinIn,
+    uril: '',
+    color: '#0072b1',
+  },
+  {
+    id: `github-${Date.now()}`,
+    Icon: FaGithub,
+    uril: '',
+    color: '#fff',
+  },
+  {
+    id: `youtube-${Date.now()}`,
+    Icon: FaYoutube,
+    uril: '',
+    color: '#ff0000',
+  },
+]
+
+export const Menus = [
+  {
+    id: `home-${Date.now()}`,
+    Icon: FaHouse,
+    uri: '#home',
+    name: 'Home',
+  },
+  {
+    id: `about-${Date.now()}`,
+    Icon: FaUser,
+    uri: '#about',
+    name: 'About',
+  },
+  {
+    id: `skills-${Date.now()}`,
+    Icon: FaRenren,
+    uri: '#skills',
+    name: 'Skills',
+  },
+  {
+    id: `projects-${Date.now()}`,
+    Icon: FaDiagramProject,
+    uri: '#projects',
+    name: 'Projects',
+  },
+  {
+    id: `contact-${Date.now()}`,
+    Icon: FaEnvelope,
+    uri: '#contact',
+    name: 'Contact',
+  },
+]
+
+export const ProjectsData = [
+  {
+    id: `carrent-${Date.now()}`,
+    name: 'Car Rent',
+    imgSrc: CarRent,
+    gitURL: 'https://github.com/frdrk00',
+  },
+  {
+    id: `bookingapp-${Date.now()}`,
+    name: 'BookingApp',
+    imgSrc: BookingApp,
+    gitURL: 'https://github.com/frdrk00',
+  },
+  {
+    id: `ecommerce-${Date.now()}`,
+    name: 'E-Commerce',
+    imgSrc: ECommerce,
+    gitURL: 'https://github.com/frdrk00',
+  },
+  {
+    id: `realtimechat-${Date.now()}`,
+    name: 'Realtime Chat',
+    imgSrc: RealtimeChat,
+    gitURL: 'https://github.com/frdrk00',
+  },
+  {
+    id: `food-${Date.now()}`,
+    name: 'Single Food Restaurant',
+    imgSrc: FoodApp,
+    gitURL: 'https://github.com/frdrk00',
+  },
+  {
+    id: `travelgoapp-${Date.now()}`,
+    name: 'TravelGoApp',
+    imgSrc: TravelGoApp,
+    gitURL: 'https://github.com/frdrk00',
+  },
+  {
+    id: `trello-${Date.now()}`,
+    name: 'Trello Clone',
+    imgSrc: TrelloApp,
+    gitURL: 'https://github.com/frdrk00',
+  },
+  {
+    id: `reddit-${Date.now()}`,
+    name: 'Reddit Clone',
+    imgSrc: RedditClone,
+    gitURL: 'https://github.com/frdrk00',
+  },
+  {
+    id: `pixabayclone-${Date.now()}`,
+    name: 'Pixabay Clone 2.0',
+    imgSrc: PixabayClone,
+    gitURL: 'https://github.com/frdrk00',
+  },
+]
